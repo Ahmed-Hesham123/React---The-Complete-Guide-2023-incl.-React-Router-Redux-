3a02454ca965b10bc922d1e2acb3a4a29925b220
Ahmed Hesham <ahmedh246891@gmail.com>
2023-08-29 04:19:18 +0300

    A new section has been added

---
+++ Section_5_React_State_and_Working_With_Events/01-starting-setup/src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+    const selectChangeHandler = (event) => {
+        props.onChangeFilter(event.target.value);
+    }
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={selectChangeHandler}>
+          <option value='2025'>2025</option>
+          <option value='2024'>2024</option>
+          <option value='2023'>2023</option>
+          <option value='2022'>2022</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
