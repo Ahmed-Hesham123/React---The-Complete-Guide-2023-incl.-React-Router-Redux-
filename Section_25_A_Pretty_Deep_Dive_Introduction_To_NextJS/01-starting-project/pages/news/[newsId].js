28b1e12d709a95d56d2d6fd6affa5763d3c6eadb
Ahmed Hesham <ahmedh246891@gmail.com>
2023-10-17 00:45:33 +0300

    A new section has been added

---
+++ Section_25_A_Pretty_Deep_Dive_Introduction_To_NextJS/01-starting-project/pages/news/[newsId].js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+
+// our-domain.com/something-important
+
+function DetailPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
+    // Send a request to the backend API
+    // to fetch the news item with newsId
+    
+    return (
+        <h1>The Detail Page</h1>
+    )
+}
+
+export default DetailPage;
+
---
+++ Section_25_A_Pretty_Deep_Dive_Introduction_To_NextJS/01-starting-project/pages/news/index.js
@@ -0,0 +1,26 @@
+// our-domain.com/news
+
+import Link from "next/link";
+import { Fragment } from "react";
+
+function NewsPage() {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/ahmed-hesham">
+            Ahmed Hesham Is A Perfect Developer
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NewsPage;
